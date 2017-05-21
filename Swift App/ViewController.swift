00eefee9827ec6f6188c129f2a8a379464ac0db4
Derek Davis <dedavis93@gmail.com>
2017-05-20 20:17:24 -0500

    My first code

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var thelabel: UILabel!
+    
+    @IBAction func buttontapped(_ sender: Any) {
+        thelabel.text = "Hello there"
+    }
+    @IBAction func button2tapped(_ sender: Any) { thelabel.text = "Hi"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+ 
+        self.view.backgroundColor = UIColor.red
     }
 
     override func didReceiveMemoryWarning() {
